58e92349ee2f008753f5a1e9fb97674810b3676c
VINICIUS PRATI MACHADO <162629373+viniprati@users.noreply.github.com>
2025-09-05 08:28:00 -0300

    .

--- Gradon/Assets/Anim/Towers/Scripts/LevelData.cs
+++ Gradon/Assets/Anim/Towers/Scripts/LevelData.cs
@@ -2,21 +2,18 @@
 
 using UnityEngine;
 
-// O atributo CreateAssetMenu nos permite criar "arquivos de fase"
-// diretamente no menu da Unity (Assets > Create > Level).
 [CreateAssetMenu(fileName = "New Level", menuName = "Tower Defense/Level")]
 public class LevelData : ScriptableObject
 {
     [Header("Informações da Fase")]
     public string levelName = "Nova Fase";
-    public int levelIndex; // Ex: 1, 2, 3...
-    public string sceneToLoad; // O nome da cena/mapa a ser carregado
+    public int levelIndex; 
+    public string sceneToLoad; 
 
     [Header("Configuração das Ondas")]
     [Tooltip("Todas as ondas de inimigos para esta fase.")]
-    public Wave[] waves; // Reutilizamos a classe 'Wave' que já criamos!
+    public Wave[] waves; 
 
     [Header("Recompensas")]
     public int manaInicial = 150;
-    // Você pode adicionar mais recompensas aqui, como "torres desbloqueadas", etc.
 }
