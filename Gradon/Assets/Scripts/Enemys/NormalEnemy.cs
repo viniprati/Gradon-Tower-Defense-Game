e96de281263655ae91ed69663571908fa38f8873
VINICIUS PRATI MACHADO <162629373+viniprati@users.noreply.github.com>
2025-09-23 08:24:27 -0300

    Update NormalEnemy.cs

--- Gradon/Assets/Scripts/Enemys/NormalEnemy.cs
+++ Gradon/Assets/Scripts/Enemys/NormalEnemy.cs
@@ -1,6 +1,4 @@
-public class NormalEnemy : Enemy
-{
-    // O ataque do inimigo normal é colidir com o totem,
-    // o que já é cuidado pelo OnCollisionEnter2D que podemos adicionar na classe base.
-    // Então, este script pode continuar vazio.
-}
+// NormalEnemy.cs
+using UnityEngine;
+
+public class NormalEnemy : Enemy { }
